f20c07c39c928aebd9722517419c925ab9ed18dd
Satvik Sabharwal <satviksabharwal7@gmail.com>
2021-01-27 15:57:53 +0530

    Integrated features with Firebase

---
+++ src/actions/auth.js
@@ -0,0 +1,13 @@
+import { firebase, googleAuthProvider } from '../firebase/firebase';
+
+export const startLogin = () => {
+    return() => {
+        return firebase.auth().signInWithPopup(googleAuthProvider);
+    }
+}
+
+export const startLogOut = () => {
+    return() => {
+        return firebase.auth().signOut();
+    }
+}
